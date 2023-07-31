7d50167ab8ed3bc1f0da33e82c65c227b846bc82
Hariom Singh Thakur <126336846+hariom-hub@users.noreply.github.com>
2023-07-31 12:55:57 +0530

    Create Uppercasing.js
    In this repository, I have created a javascript program to Uppercase the words entered by the user.

---
+++ Uppercasing.js
@@ -0,0 +1,7 @@
+/* let letters=prompt("enter a word: ");
+function upperCase(letters)
+{
+    let upper=letters.toUpperCase();
+    alert(`the uppercase of the word entered is :${upper}`);
+}
+upperCase(letters);
